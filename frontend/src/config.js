3f083be7177faafa859ae09ee0926aa184d31d0e
Ajeet Kumar <128215988+ajeet72@users.noreply.github.com>
2025-06-08 12:42:45 +0530

    Update config.js
    backend url is changed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 const config = {
-    backendUrl: "https://paytm-yxi7.onrender.com", // Replace with your actual backend URL
+    backendUrl: "https://paytm-629n.onrender.com", // Replace with your actual backend URL
   };
   
 export default config;
